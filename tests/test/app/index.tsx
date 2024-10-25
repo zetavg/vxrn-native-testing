bed771b67a250fcb0e7f69458429a9cf11390380
Pokai Chang <mail@zeta.vg>
2024-10-26 05:22:52 +0800

    test test

--- tests/test/app/index.tsx
+++ tests/test/app/index.tsx
@@ -4,6 +4,7 @@ import { Text, View } from 'react-native'
 export default () => (
   <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
     <Text>Welcome to VXS</Text>
+    <Text>Hello World</Text>
     <Link href="/sub-page/sub">Go to sub</Link>
   </View>
 )
